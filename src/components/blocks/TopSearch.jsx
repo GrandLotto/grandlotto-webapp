9a8604da23195ecfbb57089c4f8553b83772f885
vickk2005 <nwakwyevictor@gmail.com>
2023-04-10 09:07:14 +0100

    fixed issues detected

--- src/components/blocks/TopSearch.jsx
+++ src/components/blocks/TopSearch.jsx
@@ -87,11 +87,22 @@ const TopSearch = () => {
       </form>
 
       {responseError ? (
-        <div className=" text-left" style={{ marginTop: -37, marginLeft: 10 }}>
-          <Reponsemessage
-            message={responseError}
-            error={responseError ? true : false}
-          />
+        <div className=" text-left" style={{ marginTop: 0, marginLeft: 10 }}>
+          <p className="inputError text-danger">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10"
+              height="10"
+              viewBox="0 0 20 20"
+              role="presentation"
+              focusable="false"
+              tabIndex="-1"
+              fill="red"
+            >
+              <path d="M10 0C4.48 0 0 4.48 0 10s4.48 10 10 10 10-4.48 10-10S15.52 0 10 0zm0 11c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1 4H9v-2h2v2z"></path>
+            </svg>
+            <span className="ml-2">{responseError}</span>
+          </p>
         </div>
       ) : null}
     </>
