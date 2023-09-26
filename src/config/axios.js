d83015b9bea331ea9b5d215d81814081e7d4b1bc
vickk2005 <nwakwyevictor@gmail.com>
2023-09-26 13:25:25 +0100

    url updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,5 +5,5 @@ const token = localStorage.getItem("appUserThemeSettingsCode") || "";
 axios.defaults.headers.common["Accept"] = "application/json";
 // axios.defaults.baseURL = "http://172.107.167.27:8083/api";
 axios.defaults.baseURL =
-  process.env.REACT_APP_BASEURL || "http://38.242.144.36/grandlottoapi";
+  process.env.REACT_APP_BASEURL || "http://38.242.144.36/grandlottoapi/api";
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
