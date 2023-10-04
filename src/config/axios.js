5337ffb7eafd3b89234eae62f9e1d9020fa378ea
vickk2005 <nwakwyevictor@gmail.com>
2023-10-04 15:59:59 +0100

    changed url

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,5 +5,6 @@ const token = localStorage.getItem("appUserThemeSettingsCode") || "";
 axios.defaults.headers.common["Accept"] = "application/json";
 // axios.defaults.baseURL = "http://172.107.167.27:8083/api";
 axios.defaults.baseURL =
-  process.env.REACT_APP_BASEURL || "http://38.242.144.36/grandlottoapi/api";
+  process.env.REACT_APP_BASEURL ||
+  "https://www.grandlotto.ng/grandlottoapi/api";
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
